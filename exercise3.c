5a8ae0f750eb9c388027f6c56d86741cef87c043
Gal Dayan <galdayan1895@gmail.com>
2020-06-06 17:41:36 +0300

    Work version. Attention: it still print duplicates. for example: BbB will print BB

--- exercise3.c
+++ exercise3.c
@@ -1,6 +1,6 @@
-﻿#include<stdio.h>
-#include<stdlib.h>
-char* CapitalsLowers(char *s1)
+﻿#include <stdio.h>
+#include <stdlib.h>
+char *CapitalsLowers(char *s1)
 {
 	int i, j, n = 0;
 	char *s2 = '\0';
@@ -8,27 +8,36 @@ char* CapitalsLowers(char *s1)
 		if (*(s1 + i) >= 'A' && *(s1 + i) <= 'Z')
 		{
 			for (j = 0; *(s1 + j) != '\0'; j++)
-				if (*(s1 + i) == *(s1 + j) + 32)
+				if (*(s1 + i) + 32 == *(s1 + j))
 				{
-					s2 = (char*)realloc(s2, (n + 1));
+					s2 = (char *)realloc(s2, (n + 1));
 					*(s2 + n) = *(s1 + i);
 					n++;
 				}
 		}
-	*(s2 + n) = '\0';
+
+	if (n > 0)
+	{
+		*(s2 + n) = '\0';
+	}
+
 	return s2;
 }
 void PrintString(char *s2)
 {
 	int i;
 	for (i = 0; *(s2 + i) != '\0'; i++)
+	{
 		putchar(*(s2 + i));
+	}
+
+	putchar('\n');
 }
+
 void main()
 {
 	char *s1[20], *s2;
 	puts("insert sentence");
-	getchar(); // Why do you use it?
 	gets(s1);
 	s2 = CapitalsLowers(s1);
 	PrintString(s2);
